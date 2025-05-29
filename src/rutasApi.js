611b2c68284e02515f68c9ac5e61b561552abf70
iyari <iyari.estrada@uabc.edu.mx>
2025-05-29 11:26:48 -0700

    Rutas dashboard

--- src/rutasApi.js
+++ src/rutasApi.js
@@ -1,10 +1,27 @@
 const host = "http://localhost:3001/";
+
+// Rutas citas
 const citas = host + "citas";
 const citasPrimeraCita = citas + "/primera-cita";
 const citasSiguienteCita = citas + "/siguiente-cita";
 const citasSiguienteEtapa = citas + "/siguiente-etapa";
 const citasGetEtapa = citas + "/etapa/";
 const getCheckCitaPrevia = citas + "/check-cita-previa/";
+
+// Rutas expedientes/usuarios
+const usuarios = host + "expedientes/usuarios";
+// Settings Card
+const getUsuario = (numeroTel) => `${usuarios}/${numeroTel}`;
+const updateUserData = (numeroTel) => `${usuarios}/${numeroTel}`;
+const updatePassword = (numeroTel) => `${usuarios}/updatePassword/${numeroTel}`;
+// Card Table
+const updateUserPhone = (numeroTel) => `${usuarios}/updatephone/${numeroTel}`;
+const deleteUsuario = (numeroTel) => `${usuarios}/${numeroTel}`;
+// Header stats
+const getAllUsuarios = usuarios + "/all";
+const getPacientes = usuarios + "/pacientes";
+// AltaUsuarios
+const createUsuario = usuarios + "/new";
 
 export {
   citas,
@@ -13,4 +30,12 @@ export {
   citasSiguienteEtapa,
   citasGetEtapa,
   getCheckCitaPrevia,
+  getUsuario,
+  updateUserData,
+  updatePassword,
+  updateUserPhone,
+  deleteUsuario,
+  getAllUsuarios,
+  getPacientes,
+  createUsuario,
 };
